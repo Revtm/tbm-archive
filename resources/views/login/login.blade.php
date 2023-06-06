06cda2c9ceae8bcd0f4a58106bc6c531fbd3f15e
Revtm <kykylizaro98@gmail.com>
2023-06-06 23:13:40 +0700

    feature(bg): enhance bg and icon

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,15 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/login/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?t={{time()}}" rel="stylesheet">
+    <link href="{{ asset('css/login/style.css') }}?t={{time()}}" rel="stylesheet">
     <link rel="icon" href="{{ asset('icon/favicon.ico') }}" size="48x48" type="image/png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lobster">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script">
     <link rel="stylesheet" href="{{ asset('css/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
-  <body class="bg-slate-200">
+  <body class="bg-slate-100">
     @include('component/header')
     <div class="container mx-auto login-container">
       <div class="login-box shadow-md rounded-md bg-white mt-20 lg:mt-16">
@@ -50,6 +50,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}?t={{time()}}"></script>
   </body>
 </html>
--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lobster">
     <title>{{ '@' . $user->name }}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?t={{time()}}" rel="stylesheet">
     <link rel="icon" href="{{ asset('icon/favicon.ico') }}" size="48x48" type="image/png">
-    <link href="{{ asset('css/user/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/user/style.css') }}?t={{time()}}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
-  <body class="bg-slate-200">
+  <body class="bg-slate-100">
     @include('component/header')
     <div class="container mx-auto main-container mt-14">
       <div class="grid grid-rows-1 grid-cols-3 shadow-md rounded-md user-profile bg-white text-sm font-serif text-gray-600 mt-4">
@@ -112,7 +112,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}?t={{time()}}"></script>
     <script>
       function copySource(source){
         navigator.clipboard.writeText(source);
